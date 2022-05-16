fd7f0d37777a7b4b5317b7eaecaddf7c72bf1d92
Nak <ratanak.rin@stonybrook.edu>
2022-05-16 22:53:54 +0900

    Implement Questions component

---
+++ src/Components/Questions.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import { useState } from 'react'
+import CircleOutlinedIcon from '@mui/icons-material/CircleOutlined';
+import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
+
+const Questions = () => {
+  const [question, setQuestion] = useState();
+
+  console.log(question);
+
+  if (question === 'multiple-choice') {
+    return (
+      <div className='questions'>
+        <textarea className='input-question'></textarea>
+        <div className='sect1'>
+          <select className='question-types' defaultValue='multiple choice' onChange={(e) => setQuestion(e.target.value)}>
+            <option value='number'>number</option>
+            <option value='boolean'>boolean</option>
+            <option value='text'>text</option>
+            <option value='multiple-choice'>multiple choice</option>
+          </select>
+          <DeleteOutlineIcon />
+        </div>
+        <div className='choice'>
+          {/* <CircleOutlinedIcon color="disabled" fontSize="small" /> */}
+          <input type="radio"></input>
+          <textarea className='input-choice'></textarea>
+        </div>
+        <div className='choice'>
+          {/* <CircleOutlinedIcon color="disabled" fontSize="small" /> */}
+          <input type="radio"></input>
+          <textarea className='input-choice'></textarea>
+        </div>
+        <div className='choice'>
+          {/* <CircleOutlinedIcon color="disabled" fontSize="small" /> */}
+          <input type="radio"></input>
+          <textarea className='input-choice'></textarea>
+        </div>
+      </div>
+    )
+  }
+  return (
+    <div className='questions'>
+      <textarea className='input-question'></textarea>
+      <div className='sect1'>
+        <select className='question-types' onChange={(e) => setQuestion(e.target.value)}>
+          <option value='number'>number</option>
+          <option value='boolean'>boolean</option>
+          <option value='text'>text</option>
+          <option value='multiple-choice'>multiple choice</option>
+        </select>
+        <DeleteOutlineIcon />
+      </div>
+    </div>
+  )
+}
+
+export default Questions
