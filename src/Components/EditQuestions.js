a85b075092d6267215f634ecd03c71382a8982c8
sjchae <sjchae416@gmail.com>
2022-05-20 15:44:18 +0900

    change website name

--- src/Components/EditQuestions.js
+++ src/Components/EditQuestions.js
@@ -8,18 +8,18 @@ function EditQuestions() {
 	const [questions, setQuestions] = useState([]);
 
 	useEffect(() => {
-    GetQuestions();
-  }, []);
+		GetQuestions();
+	}, []);
 
 	const GetQuestions = () => {
-    fetch("http://localhost:5000/api/questions")
-      .then(res => res.json())
-      .then(data => setQuestions(data))
-      .catch(err => console.error("Error: ", err));
-  }
+		fetch('http://localhost:5000/api/questions')
+			.then((res) => res.json())
+			.then((data) => setQuestions(data))
+			.catch((err) => console.error('Error: ', err));
+	};
 
 	const editText = (newText, id) => {
-		for (var i=0; i<questions.length; i++) {
+		for (var i = 0; i < questions.length; i++) {
 			if (questions[i]._id === id) {
 				questions[i].text = newText;
 				const data = {
@@ -27,16 +27,16 @@ function EditQuestions() {
 					text: newText,
 					type: questions[i].type,
 					date: questions[i].date,
-					choices: questions[i].choices
-				}
+					choices: questions[i].choices,
+				};
 				saveUpdatedQuestion(data);
 			}
 		}
 		setQuestions([...questions]);
-	}
+	};
 
 	const editType = (type, id) => {
-		for (var i=0; i<questions.length; i++) {
+		for (var i = 0; i < questions.length; i++) {
 			if (questions[i]._id === id) {
 				questions[i].type = type;
 				const data = {
@@ -44,16 +44,16 @@ function EditQuestions() {
 					text: questions[i].text,
 					type: type,
 					date: questions[i].date,
-					choices: questions[i].choices
-				}
+					choices: questions[i].choices,
+				};
 				saveUpdatedQuestion(data);
 			}
 		}
 		setQuestions([...questions]);
-	}
+	};
 
 	const editChoices0 = (option, id) => {
-		for (var i=0; i<questions.length; i++) {
+		for (var i = 0; i < questions.length; i++) {
 			if (questions[i]._id === id) {
 				questions[i].choices[0] = option;
 				const data = {
@@ -61,16 +61,16 @@ function EditQuestions() {
 					text: questions[i].text,
 					type: questions[i].type,
 					date: questions[i].date,
-					choices: [option]
-				}
+					choices: [option],
+				};
 				saveUpdatedQuestion(data);
 			}
 		}
 		setQuestions([...questions]);
-	}
+	};
 
 	const editChoices1 = (option, id) => {
-		for (var i=0; i<questions.length; i++) {
+		for (var i = 0; i < questions.length; i++) {
 			if (questions[i]._id === id) {
 				questions[i].choices[1] = option;
 				const data = {
@@ -78,16 +78,16 @@ function EditQuestions() {
 					text: questions[i].text,
 					type: questions[i].type,
 					date: questions[i].date,
-					choices: [...questions[i].choices, option]
-				}
+					choices: [...questions[i].choices, option],
+				};
 				saveUpdatedQuestion(data);
 			}
 		}
 		setQuestions([...questions]);
-	}
+	};
 
 	const editChoices2 = (option, id) => {
-		for (var i=0; i<questions.length; i++) {
+		for (var i = 0; i < questions.length; i++) {
 			if (questions[i]._id === id) {
 				questions[i].choices[2] = option;
 				const data = {
@@ -95,108 +95,157 @@ function EditQuestions() {
 					text: questions[i].text,
 					type: questions[i].type,
 					date: questions[i].date,
-					choices: [...questions[i].choices, option]
-				}
+					choices: [...questions[i].choices, option],
+				};
 				saveUpdatedQuestion(data);
 			}
 		}
 		setQuestions([...questions]);
-	}
+	};
 
 	const addQuestion = async () => {
-		const data = await fetch("http://localhost:5000/api/questions", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        text: "",
-				type: "number",
-				date: new Date().getDay(),
-				choices: ["", "", ""]
-      })
-    }).then(res => res.json());
-
-		setQuestions([data, ...questions]);
-	}
-
-	// const updateQuestion = (updatedQuestion) => {
-  //   const updatedQuestionsArray = questions.map((question) => {
-  //     if (question._id === updatedQuestion._id) {
-  //       return updatedQuestion;
-  //     }
-  //     return question;
-  //   });
-
-  //   setQuestions(updatedQuestionsArray);
-	// 	saveUpdatedQuestion()
-  // };
-
-	const saveUpdatedQuestion = async (updates) => {
-		const data = await fetch("http://localhost:5000/api/questions/" + updates._id, {
-			method: "PUT",
+		const data = await fetch('http://localhost:5000/api/questions', {
+			method: 'POST',
 			headers: {
-				"Content-Type": "application/json"
+				'Content-Type': 'application/json',
 			},
 			body: JSON.stringify({
-				text: updates.text,
-				type: updates.type,
-				date: updates.date,
-				choices: updates.choices
-			})
-		}).then(res => res.json());
-  }
+				text: '',
+				type: 'number',
+				date: new Date().getDay(),
+				choices: ['', '', ''],
+			}),
+		}).then((res) => res.json());
 
-	const deleteQuestion = async idToDelete => {
-    const data = await fetch("http://localhost:5000/api/questions/" + idToDelete, {
-      method: "DELETE"
-    }).then(res => res.json());
+		setQuestions([data, ...questions]);
+	};
+
+	// const updateQuestion = (updatedQuestion) => {
+	//   const updatedQuestionsArray = questions.map((question) => {
+	//     if (question._id === updatedQuestion._id) {
+	//       return updatedQuestion;
+	//     }
+	//     return question;
+	//   });
+
+	//   setQuestions(updatedQuestionsArray);
+	// 	saveUpdatedQuestion()
+	// };
+
+	const saveUpdatedQuestion = async (updates) => {
+		const data = await fetch(
+			'http://localhost:5000/api/questions/' + updates._id,
+			{
+				method: 'PUT',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify({
+					text: updates.text,
+					type: updates.type,
+					date: updates.date,
+					choices: updates.choices,
+				}),
+			}
+		).then((res) => res.json());
+	};
+
+	const deleteQuestion = async (idToDelete) => {
+		const data = await fetch(
+			'http://localhost:5000/api/questions/' + idToDelete,
+			{
+				method: 'DELETE',
+			}
+		).then((res) => res.json());
 		setQuestions(questions.filter((question) => question._id !== idToDelete));
-  };
+	};
 
 	return (
 		<div className="edit-questions">
 			<div className="sect1">
 				<h2 className="main-title">Edit Questions</h2>
 				<div>
-					<AddCircleOutlineIcon className="materialIcons" onClick={addQuestion} />
+					<AddCircleOutlineIcon
+						className="materialIcons"
+						onClick={addQuestion}
+					/>
 				</div>
 			</div>
 
 			<div className="sect2">
 				{questions.map((question) => (
-					<div className='questions' key={question._id}>
-						<textarea className='input-question' onChange={(e) => editText(e.target.value, question._id)}>{question.text}</textarea>
-						<div className='sect1'>
-							<select className='question-types' defaultValue={question.type} onChange={(e) => editType(e.target.value, question._id)}>
-								<option value='number'>number</option>
-								<option value='boolean'>boolean</option>
-								<option value='text'>text</option>
-								<option value='multiple-choice'>multiple choice</option>
+					<div className="questions" key={question._id}>
+						<textarea
+							className="input-question"
+							onChange={(e) => editText(e.target.value, question._id)}
+						>
+							{question.text}
+						</textarea>
+						<div className="sect1">
+							<select
+								className="question-types"
+								defaultValue={question.type}
+								onChange={(e) => editType(e.target.value, question._id)}
+							>
+								<option value="number">number</option>
+								<option value="boolean">boolean</option>
+								<option value="text">text</option>
+								<option value="multiple-choice">multiple choice</option>
 							</select>
-							<div><DeleteOutlineIcon onClick={(e) => deleteQuestion(question._id)}/></div>
+							<div>
+								<DeleteOutlineIcon
+									onClick={(e) => deleteQuestion(question._id)}
+								/>
+							</div>
 						</div>
-						<div className={`choice ${question.type === 'multiple-choice' && "active"}`}>
+						<div
+							className={`choice ${
+								question.type === 'multiple-choice' && 'active'
+							}`}
+						>
 							<input type="radio"></input>
-							<textarea className='input-choice' onChange={(e) => editChoices0(e.target.value, question._id)}>{question.choices[0]}</textarea>
+							<textarea
+								className="input-choice"
+								onChange={(e) => editChoices0(e.target.value, question._id)}
+							>
+								{question.choices[0]}
+							</textarea>
 						</div>
-						<div className={`choice ${question.type === 'multiple-choice' && "active"}`}>
+						<div
+							className={`choice ${
+								question.type === 'multiple-choice' && 'active'
+							}`}
+						>
 							<input type="radio"></input>
-							<textarea className='input-choice' onChange={(e) => editChoices1(e.target.value, question._id)}>{question.choices[1]}</textarea>
+							<textarea
+								className="input-choice"
+								onChange={(e) => editChoices1(e.target.value, question._id)}
+							>
+								{question.choices[1]}
+							</textarea>
 						</div>
-						<div className={`choice ${question.type === 'multiple-choice' && "active"}`}>
+						<div
+							className={`choice ${
+								question.type === 'multiple-choice' && 'active'
+							}`}
+						>
 							<input type="radio"></input>
-							<textarea className='input-choice' onChange={(e) => editChoices2(e.target.value, question._id)}>{question.choices[2]}</textarea>
+							<textarea
+								className="input-choice"
+								onChange={(e) => editChoices2(e.target.value, question._id)}
+							>
+								{question.choices[2]}
+							</textarea>
 						</div>
 					</div>
 				))}
 			</div>
-			
+
 			<input
-					className="save-questions"
-					type="submit"
-					value="Save"
-					onClick={saveUpdatedQuestion}
+				className="save-questions"
+				type="submit"
+				value="Save"
+				onClick={saveUpdatedQuestion}
 			/>
 		</div>
 	);
